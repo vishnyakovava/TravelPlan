2e26a10184e3dae455c38aea3e715d2bd5dbc245
Vladimir Sukmanov <33483821+tyzik89@users.noreply.github.com>
2018-01-13 01:25:08 +0300

    Firs Commit

---
+++ TravelPlanner/src/main/java/ru/netcracker/TravelPlanner/TravelPlannerApplication.java
@@ -0,0 +1,12 @@
+package ru.netcracker.TravelPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelPlannerApplication.class, args);
+	}
+}
